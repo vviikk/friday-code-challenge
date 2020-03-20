52613bc921b274af027c6b3e708048a7fc531a9e
Vik Ramanujam <vik@ramanuj.am>
2020-03-20 17:52:44 +0000

    Implement Hello World

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -1,1 +1,8 @@
-alert('');
+// Import lit-html
+import {html, render} from 'lit-html';
+
+// Define a template
+const myTemplate = (name) => html`<p>Hello ${name}</p>`;
+
+// Render the template to the document
+render(myTemplate('World'), document.getElementById('main'));
