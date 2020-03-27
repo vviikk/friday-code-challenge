3c23405dfb405127b707a2542c2a5ab3076646d1
Vik Ramanujam <vik@ramanuj.am>
2020-03-27 00:43:32 +0000

    Randomize photos

--- packages/web/src/components/vehicle.js
+++ packages/web/src/components/vehicle.js
@@ -1,24 +1,14 @@
 import { html } from 'lit-html';
 
-const testCar = {
-  make: 'Ford',
-  model: 'Fiesta',
-  enginePowerPS: 54,
-  enginePowerKW: 40,
-  fuelType: 'Diesel',
-  bodyType: 'Limousine',
-  engineCapacity: 1119,
-};
-
-const renderRow = ([label, value]) => html`
+const renderRow = ([label, value]) => html `
   <dt>${label.replace(/([A-Z]+)/g, ' $1').replace(/([A-Z][a-z])/g, ' $1')}</dt>
   <dd>${value}</dd>`;
 
-export default ({ make, model, ...car } = testCar) => html`
+export default ({ make, model, ...car }) => html `
   <li class="cards__item">
     <div class="card">
       <header class="card__image">
-        <img src="https://picsum.photos/500/300/?image=10">
+        <img src="https://picsum.photos/500/300/?image=${Math.floor(Math.random() * 100)}">
       </header>
       <div class="card__content">
         <h1>${make} ${model}</h1>
